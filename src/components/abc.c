77bc0ad57c4f0dbc6bf4697445e84c916989778c
Abhay Gupta <76654229+abhay1088@users.noreply.github.com>
2024-05-05 15:59:22 +0530

    ch

---
+++ src/components/abc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<graphic.h>
+#include<conio.h>
+using namespace std;
+
+void main()
+{
+	int gd =DETECT,gm,x,y;
+    initgraph(&gd,&gm,"")
+
+	getch();
+	closegraph();
+	return 0;
+}
